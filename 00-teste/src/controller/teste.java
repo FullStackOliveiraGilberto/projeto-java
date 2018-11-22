485103070693310e1fdb99aff53f780dde6ec61d
FullStackOliveiraGilberto <skwcontadores@gmail.com>
2018-11-22 18:57:49 -0200

    teste quinta feira

--- 00-teste/src/controller/teste.java
+++ 00-teste/src/controller/teste.java
@@ -4,6 +4,12 @@ public class teste {
 	
 	public static void main(String[] args) {
 		System.out.println("ola mundo");
+		System.out.println("ola mundo");
+		System.out.println("ola mundo");
+		System.out.println("ola mundo");
+		System.out.println("ola mundo");
+		System.out.println("ola mundo");
+		
 	}
 
 }
